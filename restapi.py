f022ea076ab3348dfa2f4a15f1902dc5cb09c201
Danila Prokhorenko <danila.prokhorenko@student.manchester.ac.uk>
2025-08-27 16:25:53 +0100

    website gets owned games from steam based on steam ID

--- restapi.py
+++ restapi.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
@@ -12,7 +13,13 @@ def index():
 
 @app.route("/stat", methods = ["GET", "POST"])
 def getOwnedGames():
+	steam_id = ""
 	if request.method == "POST":
 		steam_id = str(request.form["SteamID"])
+		ownedGamesURL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=C12F3831CCC6A47C1FE4D87E45194A83&steamid=" + steam_id + "&format=json"
+		r = requests.get(ownedGamesURL)
+		print(r.json())
+	else:
+		print("Error")
 	return render_template("stats.html", id=steam_id)
 
