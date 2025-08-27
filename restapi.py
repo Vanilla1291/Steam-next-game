7f269d7cffe4fb76b45f627865769ba1960cb004
Danila Prokhorenko <danila.prokhorenko@student.manchester.ac.uk>
2025-08-27 16:01:30 +0100

    set up flask so now it loads the webpages and links them

--- restapi.py
+++ restapi.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/stats")
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+	return render_template("index.html")
+
+
+@app.route("/stat", methods = ["GET", "POST"])
 def getOwnedGames():
-	return "<p>Hello</p>"
+	if request.method == "POST":
+		steam_id = str(request.form["SteamID"])
+	return render_template("stats.html", id=steam_id)
 
